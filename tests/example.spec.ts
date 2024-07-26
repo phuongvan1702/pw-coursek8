8020b9a3822763618c233adc1201073bdfd12276
phuong <vanphuong1712@gmail.com>
2024-07-26 23:26:49 +0700

    update

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,20 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('verify title', async ({ page }) => {
+  await page.goto('https://pw-blog.congcu.org/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Học automation test từ chưa biết gì/);
+});
+
+test('verify started link', async ({ page }) => {
+  await page.goto('https://pw-blog.congcu.org/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Khóa học automation test cho người chưa biết gì' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Thông tin khoá học' })).toBeVisible();
+});
